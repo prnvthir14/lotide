8a697a989ba14fa0790224a4534b82a67cdf6c20
prnvthir14 <prnvthir@gmail.com>
2021-01-07 20:55:39 +0000

    replicating the map function

---
+++ map.js
@@ -0,0 +1,29 @@
+//defining a map function
+//function takes 2 parameters (arrayToMap, callback)
+
+//return a new array based on the results of the callback function.
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = (arrayToMap, callback) => {
+
+  const results = [];
+
+  for (let item of arrayToMap){
+
+    //console.log('item BEFORE: ', item);
+    
+    results.push(callback(item));
+    
+    
+    //console.log('---');
+
+  }
+
+
+  return results;
+
+}
+
+
