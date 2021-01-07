f614fdb50a8b6d390d0efa641c2cf8c8b7656ae8
prnvthir14 <prnvthir@gmail.com>
2021-01-07 01:58:58 +0000

    function to iterate through a string and return an object whose KV pairs represent the occurence of each letter in the string

---
+++ countLetters.js
@@ -0,0 +1,49 @@
+
+const assertEqual = (actual, expected) => {
+
+  if (actual === expected) {
+   
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  
+  } else {
+
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+
+};
+
+
+//The function should take in a sentence (as a string)  
+//return a count of each of the letters in that sentence.
+
+const countLetters = function(senentce) {
+
+  let returnObject = {};
+  //need to remove spaces from input
+  let refinedString = senentce.split(" ").join("")
+  console.log(refinedString);
+
+  for (let x of refinedString){
+    //check that we loop through the whole sentence: WORKS
+    // console.log(senentce[x]);
+    //console.log(!returnObject[x])
+    // !returnObject[x] - CHECKING IF THE KEY EXISTS OR NOT.
+    //Not filtering anything in this example 
+    if (!returnObject[x]){
+      //if the key does not exist, add the key to your return object and assign it an initial value = 1
+      returnObject[x]=1;
+
+    } else {
+      //else if the key does exist, increment its value by 1 and we move on to the next element.
+      returnObject[x]+=1;
+
+    }
+    
+  }  
+  return returnObject;
+}
+
+console.log(countLetters('lighthouse in the house'));
+console.log(countLetters('LHL'));
+console.log(countLetters('where did I go wrong'));
