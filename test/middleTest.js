d06ce725e6e3261c8736936d2396e8c10196ddac
pranav <prnvthir@gmail.com>
2021-01-11 15:21:29 -0500

    middleTest now incorporates M&C testing framework

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,24 +1,47 @@
-const assertEqual = require('../assertEqual.js');
-const eqArrays = require('../eqArrays.js');
-const middle = require('../middle');
-// const assertArraysEqual = require ('../assertArraysEqual')
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
-const assertArraysEqual = require('../assertArraysEqual');
+describe("#middle", () => {
 
-// //check: returns nothing when there are 2 or less elements in the array;
-// console.log(middle([7,8]));
-// console.log(middle([7]));
-// console.log(middle([]));
+  it("returns [] for [[7,8]]", () => {
+    assert.deepEqual(middle([[7,8]]), []);
+  });
+
+  it("returns [] for [7]", () => {
+    assert.deepEqual(middle([[7]]), []);
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([[]]), []);
+  });
+
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(middle(['5']), []); 
+  });
+
+  it("returns [9] for [7,8,9,10,11]", () => {
+    assert.deepEqual(middle([7,8,9,10,11]),[9]); 
+  });
+
+  it("returns [9,10] for [7,8,9,10,11,12]", () => {
+    assert.deepEqual(middle([7,8,9,10,11,12]),[9,10]); 
+  });
 
 
-// //check: returns middle element for an array with an odd# of elements
-(middle([7,8,9,10,11]), [9]);
+});
 
-// //check: returns middle 2 elements for an array with an even# of elements
-// assertArraysEqual(middle([7,8,9,10,11]), [9,10])
+
+// // //check: returns middle element for an array with an odd# of elements
+// (middle([]), [9]);
+
+// // //check: returns middle 2 elements for an array with an even# of elements
+// assertArraysEqual(middle([7,8,9,10,11,12]), [9,10])
 
 
 // //takes 2 parameters, actual and expected.
 // //parameter 1 = middle[]
 // //parameter 2 = the expected answer of calling middle
-assertArraysEqual(middle([7,8,9,10,11]), [9]) // should return true
+//assertArraysEqual(middle([7,8,9,10,11]), [9]) // should return true
+
+
+
