ea330c69c2135478950011e3dc68679796c16917
pranav <prnvthir@gmail.com>
2021-01-11 15:10:35 -0500

    added logic to handle empty arrays

--- tail.js
+++ tail.js
@@ -5,18 +5,31 @@ const tail = (array) => {
   //initilaizing empty tail array
   let tailArray = [];
 
-  //loop through input array starting with index = 1 and push to tail
-  for (let i = 1; i < array.length; i++) {
+  if (array.length < 1){
+
+    return 'empty array';
+
+  } else if (array.length === 1){
+
+    tailArray = [];
+
+  } else if (array.length > 1) {
+    
+    //loop through input array starting with index = 1 and push to tail
+    for (let i = 1; i < array.length; i++) {
 
     tailArray.push(array[i]);
+    
+    }  
+    return tailArray;
   
   }
-  
+ 
   //need to return the tail of the array
   return tailArray;
   
 };
 
 
-module.exports = tail;
 
+module.exports = tail;
