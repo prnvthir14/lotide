9c55d34532da31c2970ced9cd01a95fb9267561d
pranav <prnvthir@gmail.com>
2021-01-11 15:13:40 -0500

    tailTest now incorporates M&C testing framework

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,25 @@
-const assertEqual = require('../assertEqual')
-const tail = require('../tail')
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]),'empty array' ); 
+  });
+
+  it("returns ['where','you'] for ['hi','where','you']", () => {
+    assert.deepEqual(tail(['hi','where','you']),['where','you'] ); 
+  });
 
 
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+});
