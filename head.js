79f87f942823dab2f7127a1ea37791292b82c747
pranav <prnvthir@gmail.com>
2021-01-11 14:32:46 -0500

    added logic for empty arrays

--- head.js
+++ head.js
@@ -5,8 +5,17 @@ const assertEqual = require('./assertEqual.js');
 const head = (array) => {
 
   //return first element of an array
-  let head = array[0];
-  return head;
+  if (array.length > 0){
+
+    let head = array[0];
+    return head;
+
+  } else {
+
+    return []
+
+  }
+
 
 }; 
 
